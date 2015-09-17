f040642da95bbba670b095dde75346873ec45884
Andrew Kuklewicz <andrew@beginsinwonder.com>
2015-09-17 13:25:23 -0400

    add routes for future api calls, use as href n hal representer links

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,11 @@
 Rails.application.routes.draw do
+
+  scope ':api_version', api_version: 'v1' do
+    resources :feeds do
+      resources :entries, controller: 'feed_entries'
+    end
+  end
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
