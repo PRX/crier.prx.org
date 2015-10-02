72a6502888f481e12e132a077f0d3cf9f4f1563f
Andrew Kuklewicz <andrew@beginsinwonder.com>
2015-10-02 11:35:29 -0400

    correctly initialize shoryuken client and server

--- config/initializers/shoryuken.rb
+++ config/initializers/shoryuken.rb
@@ -1,10 +1,23 @@
 require 'shoryuken'
+require 'shoryuken/extensions/active_job_adapter'
 
-config_file = File.join(Rails.root, 'config', 'shoryuken.yml')
-Shoryuken::EnvironmentLoader.load(config_file: config_file)
+Shoryuken.default_worker_options =  {
+  'queue'                   => "#{Rails.env}_crier_default",
+  'auto_delete'             => true,
+  'auto_visibility_timeout' => true,
+  'batch'                   => false,
+  'body_parser'             => :json
+}
 
 Shoryuken.configure_server do |config|
   Rails.logger = Shoryuken::Logging.logger
+  ActiveJob::Base.logger = Shoryuken::Logging.logger
+  ActiveRecord::Base.logger = Shoryuken::Logging.logger
 end
 
-Shoryuken.active_job_queue_name_prefixing = true
+Shoryuken.configure_client do |config|
+  unless Rails.env.test?
+    config_file = File.join(Rails.root, 'config', 'shoryuken.yml')
+    Shoryuken::EnvironmentLoader.load(config_file: config_file)
+  end
+end
