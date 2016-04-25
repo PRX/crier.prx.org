0d716ceccb6155fa856fea4cb31dc05e3b4270eb
Daniel Friedman <dfriedman58@gmail.com>
2016-04-25 10:03:31 -0400

    Revert migration 20160425135331_add_enclosure_etag_to_feed_entries

--- db/migrate/20160425135331_add_enclosure_etag_to_feed_entries.rb
+++
@@ -1,5 +0,0 @@
-class AddEnclosureEtagToFeedEntries < ActiveRecord::Migration
-  def change
-    add_column :feed_entries, :enclosure_etag, :string
-  end
-end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160425135331) do
+ActiveRecord::Schema.define(version: 20151101205144) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -46,7 +46,6 @@ ActiveRecord::Schema.define(version: 20160425135331) do
     t.string   "feedburner_orig_enclosure_link"
     t.datetime "deleted_at"
     t.boolean  "is_perma_link"
-    t.string   "enclosure_etag"
   end
 
   add_index "feed_entries", ["deleted_at"], name: "index_feed_entries_on_deleted_at", using: :btree
