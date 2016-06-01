6f3733b9020ad50da82bb7783118b44f7f5748ac
Andrew Kuklewicz <andrew@beginsinwonder.com>
2016-06-01 17:48:26 -0400

    read only api

--- config/routes.rb
+++ config/routes.rb
@@ -8,10 +8,10 @@ Rails.application.routes.draw do
       root to: 'base#entrypoint'
       match '*any', via: [:options], to: 'base#options'
 
-      resources :feeds do
-        resources :entries, controller: 'feed_entries', except: [:new, :edit]
+      resources :feeds, only: [:show, :index] do
+        resources :entries, controller: 'feed_entries', only: [:show, :index]
       end
-      resources :entries, controller: 'feed_entries'
+      resources :entries, controller: 'feed_entries', only: [:show, :index]
     end
   end
 
