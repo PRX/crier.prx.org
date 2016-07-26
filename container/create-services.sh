f4d4b7097580036d9622d661e9b2d75a836af03c
Andrew Kuklewicz <andrew@beginsinwonder.com>
2016-07-26 10:06:51 -0400

    create services and elb with the script

--- container/create-services.sh
+++ container/create-services.sh
@@ -54,11 +54,13 @@ do
   render_template $tf | jq "." > $of
 
   # create the cloudwatch setup
-  echo aws logs create-log-group --log-group-name ecs-${service} --region ${DEPLOY_AWS_REGION}
-  echo aws logs put-retention-policy --log-group-name ecs-${service} --retention-in-days ${LOG_RETENTION_DAYS}
+  echo "aws logs"
+  aws logs create-log-group --log-group-name ecs-${service} --region ${DEPLOY_AWS_REGION}
+  aws logs put-retention-policy --log-group-name ecs-${service} --retention-in-days ${LOG_RETENTION_DAYS}
 
   # create the task def
-  echo aws ecs register-task-definition --cli-input-json file://$of
+  echo "aws register task definition"
+  aws ecs register-task-definition --cli-input-json file://$of
 
   # if this is a `web` service, create an elb
   loadBalancerOption=
@@ -75,22 +77,34 @@ do
     certArn=$(aws acm list-certificates | jq -r ".CertificateSummaryList[] | select(.DomainName|endswith(\"${domainName}\")) | .CertificateArn")
     listeners="Protocol=HTTP,LoadBalancerPort=80,InstanceProtocol=HTTP,InstancePort=$HOST_PORT Protocol=HTTPS,LoadBalancerPort=443,InstanceProtocol=HTTP,InstancePort=$HOST_PORT,SSLCertificateId=${certArn}"
 
-    echo aws elb create-load-balancer \
+    echo "create load balancer"
+    aws elb create-load-balancer \
       --load-balancer-name ${service} \
       --listeners ${listeners} \
       --subnets ${subnets} \
       --security-groups ${securityGroups} \
-      --tags Name=Name,Value=${service} Name=Environment,Value=$ENV
+      --tags Key=Name,Value=${service} Key=Environment,Value=$ENV
 
     loadBalancerOption=loadBalancerName=${service},containerName=${BASE_NAME}-${bn},containerPort=$CONTAINER_PORT
+
+    # create the service
+    echo "create web service"
+    aws ecs create-service \
+      --cluster prx-${environment} \
+      --service-name ${service} \
+      --task-definition ${service} \
+      --load-balancers ${loadBalancerOption}\
+      --desired-count $DESIRED_COUNT \
+      --role ecsServiceRole
+  else
+
+    # create the service
+    echo "create service"
+    aws ecs create-service \
+      --cluster prx-${environment} \
+      --service-name ${service} \
+      --task-definition ${service} \
+      --desired-count $DESIRED_COUNT
   fi
 
-  # create the service
-  echo aws create-service \
-    --cluster prx-${environment} \
-    --service-name ${service} \
-    --task-definition ${service} \
-    --load-balancers ${loadBalancerOption}\
-    --desired-count $DESIRED_COUNT \
-    --role ecsServiceRole
 done
