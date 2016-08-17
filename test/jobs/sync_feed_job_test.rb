c1b8b5584ff97ec19e2883aa9ea9d87dad174ddb
cavis <ryancavis@gmail.com>
2016-08-17 14:17:09 -0600

    Don't reschedule thrown errors

--- test/jobs/sync_feed_job_test.rb
+++ test/jobs/sync_feed_job_test.rb
@@ -7,19 +7,51 @@ describe SyncFeedJob do
       to_return(status: 200, body: test_file('/fixtures/99percentinvisible.xml'), headers: {})
     stub_head_requests(/http:\/\/.*\.podtrac.com\/.*/)
   }
-  let(:feed) { Feed.create!(feed_url: 'http://feeds.99percentinvisible.org/99percentinvisible') }
+  let(:job) { SyncFeedJob.new }
+  let(:feed) do
+    f = Feed.create!(feed_url: 'http://feeds.99percentinvisible.org/99percentinvisible')
+    clear_enqueued_jobs # clear the initial sync
+    f
+  end
 
-  it 'sync feed by id' do
-    SyncFeedJob.new.perform(feed, true)
+  it 'syncs and reschedules feeds' do
+    assert_enqueued_jobs 0
+    feed.responses.count.must_equal 0
+    job.perform(feed, true)
+    assert_enqueued_jobs 1
+    feed.responses.count.must_equal 1
+  end
+
+  it 'does not sync or reschedule deleted feeds' do
+    feed.destroy!
+    assert_enqueued_jobs 0
+    feed.responses.count.must_equal 0
+    job.perform(feed, true)
+    assert_enqueued_jobs 0
+    feed.responses.count.must_equal 0
   end
 
   it 'does not schedule jobs if unschedule false' do
-    SyncFeedJob.new.perform(feed, false)
+    assert_enqueued_jobs 0
+    feed.responses.count.must_equal 0
+    job.perform(feed, false)
+    assert_enqueued_jobs 0
+    feed.responses.count.must_equal 1
   end
 
-  it 'does not schedule jobs with a deleted feed' do
-    feed.stub(:deleted?, true) do
-      SyncFeedJob.new.perform(feed, true)
+  it 'does not reschedule jobs that throw an error' do
+    raises_exception = -> { raise ArgumentError.new }
+    feed.stub(:sync, raises_exception) do
+      assert_enqueued_jobs 0
+      feed.responses.count.must_equal 0
+      begin
+        job.perform(feed, true)
+        raise 'should not have gotten here'
+      rescue ArgumentError => e
+        assert_enqueued_jobs 0
+        feed.responses.count.must_equal 0
+      end
     end
   end
+
 end
